5890c6a978fb399147e90fa6b683bc0b3192b8c7
atulzambre <atul.zambre123@gmail.com>
2019-11-30 02:50:11 +0530

    Singly Linked List delete Pos

---
+++ src/main/java/com/datastructures/linkedlist/singlylinkedlist/SinglyLinkedListDeletePos.java
@@ -0,0 +1,69 @@
+package com.datastructures.linkedlist.singlylinkedlist;
+
+/**
+ Given a singly linked list and a position, delete a linked list node at the given position.
+
+ Example:
+
+ Input: position = 1, Linked List = 8->2->3->1->7
+ Output: Linked List =  8->3->1->7
+
+ Input: position = 0, Linked List = 8->2->3->1->7
+ Output: Linked List = 2->3->1->7
+
+ */
+
+public class SinglyLinkedListDeletePos {
+
+    Node head;
+
+    public static class Node{
+        int data;
+        Node next;
+
+        Node(int data){
+            this.data=data;
+            next=null;
+        }
+    }
+
+
+    public static void main(String[] args) {
+        SinglyLinkedListDeletePos singlyLinkedListDeletePos=new SinglyLinkedListDeletePos();
+        singlyLinkedListDeletePos.head=new Node(1);
+        Node newNode=new Node(4);
+        singlyLinkedListDeletePos.head.next=newNode;
+        singlyLinkedListDeletePos.printList();
+        singlyLinkedListDeletePos.deletePOS(1);
+        singlyLinkedListDeletePos.printList();
+
+    }
+
+    private void deletePOS(int pos){
+        int posCount=0;
+
+        Node checkNode=head;
+        Node prevNode=head;
+        if(pos==0) {
+            head = head.next;
+            return;
+        }
+        while(checkNode!=null){
+            if(posCount==pos){
+                prevNode.next=checkNode.next;
+                break;
+            }
+            prevNode=checkNode;
+            checkNode=checkNode.next;
+            posCount++;
+        }
+    }
+
+    private void printList(){
+        Node printNode=head;
+        while(printNode!=null){
+            System.out.println(printNode.data);
+            printNode=printNode.next;
+        }
+    }
+}
