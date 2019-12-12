c01db711d02197daaa08bc7aab77b5f3609f1f96
atulzambre <atul.zambre123@gmail.com>
2019-12-12 23:26:01 +0530

    Doubly Linked List

--- src/main/java/com/datastructures/linkedlist/doublylinkedlist/DoublyLinkedListReverse.java
+++ src/main/java/com/datastructures/linkedlist/doublylinkedlist/DoublyLinkedListReverse.java
@@ -8,15 +8,40 @@ public class DoublyLinkedListReverse {
 
     public static void main(String[] args) {
         DoublyLinkedListReverse doublyLinkedListReverse = new DoublyLinkedListReverse();
+        doublyLinkedListReverse.push(1);
+        doublyLinkedListReverse.push(2);
+        doublyLinkedListReverse.push(3);
+        doublyLinkedListReverse.push(4);
+        doublyLinkedListReverse.printList();
+        System.out.println("\n---------------After Reversing-------------");
+        doublyLinkedListReverse.reverse();
+        doublyLinkedListReverse.printList();
+
+    }
+
+    private void reverse() {
+        Node checkNode = head;
+        while (checkNode != null) {
+            Node temp = checkNode.prev;
+            checkNode.prev = checkNode.next;
+            checkNode.next = temp;
+            head = checkNode;
+            checkNode = checkNode.prev;
+
+        }
+
 
     }
 
     private void push(int data) {
         Node newNode = new Node(data);
-        if (head == null)
+        Node checkNode = head;
+        if (checkNode != null) {
+            newNode.next = checkNode;
+            checkNode.prev = newNode;
             head = newNode;
-        else
-            newNode.next = head;
+        } else
+            head = newNode;
     }
 
     private void printList() {
