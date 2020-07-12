c81db9b6f75bbc872b4d38475746e07f11d5c17e
atulzambre <atul.zambre123@gmail.com>
2020-07-12 23:11:48 +0530

    resolved conflict

--- src/main/java/com/datastructures/arrays/arrayrotation/TestRahul.java
+++ src/main/java/com/datastructures/arrays/arrayrotation/TestRahul.java
@@ -2,6 +2,6 @@ package com.datastructures.arrays.arrayrotation;
 
 public class TestRahul {
     public static void main(String[] args) {
-        System.out.println("Rahul bhaiya");
+        System.out.println("Rahul zambre");
     }
 }
